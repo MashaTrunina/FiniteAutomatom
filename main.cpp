396d786458b0e2737d1077387ad038273d58f06f
MashaTrunina <147832180+MashaTrunina@users.noreply.github.com>
2023-12-29 10:48:38 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,35 +4,32 @@
 #include "Header.h"
 
 int main() {
-
-    setlocale(LC_ALL, "RU");
-
     std::unordered_set<std::string> states = { "1", "2", "3", "4" };
     std::unordered_set<std::string> alphabet = { "a", "b" };
     std::string initialState = "1";
     std::unordered_set<std::string> finalStates = { "4" };
     std::vector<Transition> transitions = {
-        { "1", "a", "2" }, { "1", "b", "4" },
-        { "2", "a", "1" }, { "2", "b", "3" },
-        { "3", "a", "4" }, { "3", "b", "2" },
-        { "4", "a", "3" }, { "4", "b", "1" },
+        { "1", { "a" }, "2" }, { "1", { "b" }, "4" },
+        { "2", { "a" }, "1" }, { "2", { "b" }, "3" },
+        { "3", { "a" }, "4" }, { "3", { "b" }, "2" },
+        { "4", { "a" }, "3" }, { "4", { "b" }, "1" },
     };
 
     FiniteAutomaton fa(states, alphabet, transitions, finalStates, initialState);
 
-    std::vector<std::string> test1 = { "b", "b", "a" };
-    std::cout << "Test 1: ";
-    bool result1 = fa.execute(test1);
-    std::cout << (result1 ? "Accepted" : "Rejected") << std::endl;
+    while (true) {
+        std::cout << "Enter a string (or 'q' to quit): ";
+        std::string input;
+        std::getline(std::cin, input);
 
-    std::vector<std::string> test2 = { "a", "b", "a", "a", "b" };
-    std::cout << "Test 2: ";
-    bool result2 = fa.execute(test2);
-    std::cout << (result2 ? "Accepted" : "Rejected") << std::endl;
+        if (input == "q") {
+            break;
+        }
 
-    std::vector<std::string> test3 = { "b", "a", "b", "a", "a", "b" };
-    std::cout << "Test 3: ";
-    bool result3 = fa.execute(test3);
-    std::cout << (result3 ? "Accepted" : "Rejected") << std::endl;
+        std::cout << "Result: ";
+        bool result = fa.acceptsString(input);
+        std::cout << (result ? "Accepted" : "Rejected") << std::endl;
+    }
+
     return 0;
 }
